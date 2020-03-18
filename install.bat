ae97dd532ed7c50f56a90644b753b718980c22cb
Thomas J  Shotts Jr <37753391+darkwolfxj@users.noreply.github.com>
2020-03-18 15:40:22 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
 set test=%~0
-copy %test%\..\touch.bat %windir%\system32\ > null 
+copy %test%\..\touch.bat %windir%\system32\ > nul
 echo Successfully installed! You can now run the Linux-only command *touch* with up to 9 files in CMD! 
-pause > null
+pause > nul
